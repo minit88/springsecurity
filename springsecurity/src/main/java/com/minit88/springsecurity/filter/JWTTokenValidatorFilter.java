6e6ff046712515e4323c97e534ae23d899f35615
minit88 <chymh32@gmail.com>
2025-01-04 22:46:21 +0900

    Feat: JWT 검사 필터 구현

--- springsecurity/src/main/java/com/minit88/springsecurity/filter/JWTTokenValidatorFilter.java
+++ springsecurity/src/main/java/com/minit88/springsecurity/filter/JWTTokenValidatorFilter.java
@@ -1,4 +1,49 @@
 package com.minit88.springsecurity.filter;
 
-public class JWTTokenValidatorFilter {
+import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class JWTTokenValidatorFilter extends OncePerRequestFilter {
+    public Dotenv dotenv = Dotenv.load();
+    private String secret = dotenv.get("SECRET_KEY");
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+            String jwt = request.getHeader("Authorization");
+            if (null != jwt){
+                SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                if (null != secretKey){
+                    Claims claims = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(jwt).getPayload();
+                    String username = String.valueOf(claims.get("username"));
+                    String authorities = String.valueOf(claims.get("authorities"));
+                    Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            throw new InternalError(e.getMessage());
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getServletPath().equals("/login"); // reqeust path가 "/login" 가 아니면 필터 적용 -> 로그인을 제외한 요청에 적용
+    }
 }
